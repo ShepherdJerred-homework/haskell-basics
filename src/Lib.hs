9b78a01607c3c55a5d417797a91d03f5e370e137
Jerred Shepherd <ShepherdJerred@gmail.com>
2019-01-17 11:14:01 -0600

    Finish impl

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,4 +1,4 @@
-module Lib (second, firstN, size) where
+module Lib (second, firstN, size, inRange, validGrade, startsWith, first10, (Lib.>>), factorial, range) where
 
 second :: [Int] -> Int
 second list = list !! 1
@@ -15,3 +15,24 @@ size list = do
       if l <= 20
         then "medium"
         else "large"
+
+inRange :: Int -> Int -> Int -> Bool
+inRange min max test = if test >= min then if test <= max then True else False else False
+
+validGrade :: Int -> Bool
+validGrade grade = inRange 0 100 grade
+
+startsWith :: Int -> [Int] -> Bool
+startsWith test list = head list == test
+
+first10 :: Int -> [Int] -> Bool
+first10 test list = test `elem` (take 10 list)
+
+(>>) :: [Int] -> [Int] -> Bool
+(>>) l r = if maximum l > maximum r then True else False
+
+factorial :: Int -> Int
+factorial n = if n == 1 then 1 else factorial (n - 1) * n
+
+range :: [Int] -> Int -> Int -> [Int]
+range list start end = drop start (take end list)
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,27 +1,73 @@
 import Lib
 import Test.Hspec
 
+import qualified Lib((>>))
+
 main :: IO ()
 
 main = hspec $ do
   describe "second" $ do
     it "returns the second element" $ do
       second [1..] `shouldBe` 2
-    it "passes test case given by Dr. Foust" $ do
       second [3, 1, 4, 1, 5, 9] `shouldBe` 1
   describe "firstN" $ do
-    it "returns expected elements" $ do
+    it "returns first n elements" $ do
       firstN 10 `shouldBe` [1..10]
-    it "passes test case given by Dr. Foust" $ do
       firstN 7 `shouldBe` [1..7]
   describe "size" $ do
-    it "returns small for 0" $ do
+    it "returns small when less than 10" $ do
       size [] `shouldBe` "small"
-    it "returns small for 9" $ do
       size [1..9] `shouldBe` "small"
-    it "returns medium for 10" $ do
+    it "returns medium when between 10-20" $ do
       size [1..10] `shouldBe` "medium"
-    it "returns medium for 20" $ do
       size [1..20] `shouldBe` "medium"
-    it "returns large for 21" $ do
+    it "returns large for 21+" $ do
       size [1..21] `shouldBe` "large"
+  describe "inRange" $ do
+    it "returns true when in range" $ do
+      inRange 1 10 5 `shouldBe` True
+      inRange 1 10 1 `shouldBe` True
+      inRange 1 10 10 `shouldBe` True
+      inRange 3 7 5 `shouldBe` True
+    it "returns false when out of range" $ do
+      inRange 1 10 11 `shouldBe` False
+      inRange 1 10 0 `shouldBe` False
+      inRange 3 7 15 `shouldBe` False
+  describe "validGrade" $ do
+    it "returns true when valid" $ do
+      validGrade 0 `shouldBe` True
+      validGrade 100 `shouldBe` True
+      validGrade 96 `shouldBe` True
+    it "returns false when invalid" $ do
+      validGrade (-1) `shouldBe` False
+      validGrade 101 `shouldBe` False
+      validGrade 196 `shouldBe` False
+  describe "startsWith" $ do
+    it "returns true when list starts with test value" $ do
+      startsWith 1 [1..] `shouldBe` True
+      startsWith 7 [7, 3, 1, 4] `shouldBe` True
+    it "returns false when list doesn't start with test value" $ do
+      startsWith 2 [1..] `shouldBe` False
+      startsWith 7 [9, 7, 7, 7] `shouldBe` False
+  describe "first10" $ do
+    it "returns true when test is in list" $ do
+      first10 1 [1..] `shouldBe` True
+      first10 10 [1..] `shouldBe` True
+      first10 8 [1..] `shouldBe` True
+    it "returns false when test is not in list" $ do
+      first10 11 [1..] `shouldBe` False
+      first10 100 [1..] `shouldBe` False
+      first10 27 [1..] `shouldBe` False
+  describe ">>" $ do
+    it "returns true when l has a max greater than r" $ do
+      [1..10] Lib.>> [1..5] `shouldBe` True
+    it "returns false when r has a max greater than l" $ do
+      [1..10] Lib.>> [1..10] `shouldBe` False
+      [1..5] Lib.>> [1..10] `shouldBe` False
+  describe "factorial" $ do
+    it "returns factorial" $ do
+      factorial 5 `shouldBe` 120
+      factorial 4 `shouldBe` 24
+  describe "range" $ do
+    it "returns numbers in range" $ do
+      range [1..] 10 20 `shouldBe` [11..20]
