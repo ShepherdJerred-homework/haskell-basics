0629cd9d3e4de7fb39b146784fb0322613317a19
Jerred Shepherd <ShepherdJerred@gmail.com>
2019-01-16 23:42:06 -0600

    Add firstN and size

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,8 +1,17 @@
-module Lib
-    (second
-    ) where
+module Lib (second, firstN, size) where
 
 second :: [Int] -> Int
 second list = list !! 1
 
+firstN :: Int -> [Int]
+firstN n = take n [1..]
 
+size :: [Int] -> String
+size list = do
+  let l = length list
+  if l < 10
+    then "small"
+    else
+      if l <= 20
+        then "medium"
+        else "large"
--- test/Spec.hs
+++ test/Spec.hs
@@ -7,3 +7,21 @@ main = hspec $ do
   describe "second" $ do
     it "returns the second element" $ do
       second [1..] `shouldBe` 2
+    it "passes test case given by Dr. Foust" $ do
+      second [3, 1, 4, 1, 5, 9] `shouldBe` 1
+  describe "firstN" $ do
+    it "returns expected elements" $ do
+      firstN 10 `shouldBe` [1..10]
+    it "passes test case given by Dr. Foust" $ do
+      firstN 7 `shouldBe` [1..7]
+  describe "size" $ do
+    it "returns small for 0" $ do
+      size [] `shouldBe` "small"
+    it "returns small for 9" $ do
+      size [1..9] `shouldBe` "small"
+    it "returns medium for 10" $ do
+      size [1..10] `shouldBe` "medium"
+    it "returns medium for 20" $ do
+      size [1..20] `shouldBe` "medium"
+    it "returns large for 21" $ do
+      size [1..21] `shouldBe` "large"
